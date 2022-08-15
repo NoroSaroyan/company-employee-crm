673d179a256dae56537b85c378ca1509d8feeac4
Norik <47327117+NoroSaroyan@users.noreply.github.com>
2022-08-16 00:23:37 +0300

    added test in GitHub action (#1)
    * added test in GitHub action
    
    * added job in workflow
    
    * added test properties
    
    * fixed
    
    * fixed

--- src/test/java/ru/project/companyemployeecrm/CompanyEmployeeCrmApplicationTests.java
+++ src/test/java/ru/project/companyemployeecrm/CompanyEmployeeCrmApplicationTests.java
@@ -2,8 +2,10 @@ package ru.project.companyemployeecrm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(value = "classpath:application-test.properties")
 class CompanyEmployeeCrmApplicationTests {
 
 	@Test
