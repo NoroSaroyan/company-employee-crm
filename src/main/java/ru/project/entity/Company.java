45dd721731ff55f3293b6affdb59f7fbbe3bdc00
Norik <noriksaroyan@gmail.com>
2022-08-15 17:36:00 +0300

    Added company and employee entities and GitHub workflow file

---
+++ src/main/java/ru/project/entity/Company.java
@@ -0,0 +1,24 @@
+package ru.project.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Table(name = "companies")
+public class Company {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name",unique = true)
+    @NotNull
+    private String name;
+
+    @Column(name = "website")
+    private String website;
+
+    @Column(name = "email")
+    private String email;
+}
---
+++ src/main/java/ru/project/entity/Employee.java
@@ -0,0 +1,4 @@
+package ru.project.entity;
+
+public class Employee {
+}
