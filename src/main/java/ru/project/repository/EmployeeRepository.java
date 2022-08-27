554370254eb9e6c97aae6fc65ea584d7424d6d63
Norik <noriksaroyan@gmail.com>
2022-08-27 17:11:04 +0300

    made changes in repository

--- src/main/java/ru/project/repository/EmployeeRepository.java
+++ src/main/java/ru/project/repository/EmployeeRepository.java
@@ -8,11 +8,15 @@ import ru.project.entity.Employee;
 import java.util.Optional;
 
 @Repository
-public interface EmployeeRepository extends CrudRepository<Employee,Long> {
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
     @Override
+    @NonNull
     Optional<Employee> findById(@NonNull Long id);
 
-    Optional<Employee> getAllByCompany_Id(Long companyId);
+    Optional<Employee> getEmployeesByCompany_Id(Long companyId);
+
+
+    Optional<Employee> findByCompany_Id(Long companyId);
 
 }
