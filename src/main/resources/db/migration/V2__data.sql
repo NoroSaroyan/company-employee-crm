759262ad091f10e6d38d798312ba38a29552748e
Norik <noriksaroyan@gmail.com>
2022-09-07 11:56:31 +0300

    changes in home controller and home view

--- src/main/resources/db/migration/V2__data.sql
+++ src/main/resources/db/migration/V2__data.sql
@@ -1,11 +1,11 @@
 INSERT INTO users(id, email, password)
-values ('1122', 'email', 'password');
+values ('1', 'admin@test.com', 'password');
 
 INSERT INTO authorities(id, name)
 values (1, 'ADMIN');
 
 INSERT INTO users_authorities(user_id, authority_id)
-values (1122, 1);
+values (1, 1);
 
 INSERT INTO companies
 VALUES (1, 'google', 'google@gmail.com', 'not provided');
@@ -18,11 +18,11 @@ INSERT INTO companies(id, name, email, website)
 VALUES (3, 'youtube', 'youtube@gmail.com', 'not provided');
 
 INSERT INTO employees(id, companyid, name, surname, phone_number, email)
-VALUES (001, 1, 'John', 'Doe', 'not provided',
+VALUES (1, 1, 'John', 'Doe', 'not provided',
         'JohnDoe@gmail.com');
 INSERT INTO employees(id, companyid, name, surname, phone_number, email)
-VALUES (002, 2, 'John', 'White', '+79001234567',
+VALUES (2, 2, 'John', 'White', '+79001234567',
         'JohnWhite@gmail.com');
 INSERT INTO employees(id, companyid, name, surname, phone_number, email)
-VALUES (003, 3, 'Jane', 'Smith', '+79001234568',
+VALUES (3, 3, 'Jane', 'Smith', '+79001234568',
         'JohnSmith@gmail.com');
